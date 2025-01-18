148157d7d9b02caaa77ab277f22da32f2668b803
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-01-19 00:11:31 +0500

    added images for readme.md

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Match any request starting with /api/
+        destination: 'https://next-ecommerce-template-4.vercel.app/api/:path*', // Forward the request to this external API
+      },
+    ];
+  },
 };
 
 export default nextConfig;
