174d106cac3008525599136093d6029f7af0f5fb
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-01-18 23:24:12 +0500

    added images for readme.md

--- src/app/sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import ProductSchema from './product';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [ProductSchema],
-};
