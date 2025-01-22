cf6bb3c57ff40f810b798f5fe9adab2733d57f3e
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-01-22 09:25:34 +0500

    solve vercel errors

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,10 +8,18 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production', // or your dataset name
+  apiVersion: '2023-05-03', // use current date
+  useCdn: true
+})
+
 
 export default defineConfig({
   basePath: '/studio',
