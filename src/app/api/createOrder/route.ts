9b802cbede9e736dd75e5696d2fa7d6d56ec68c7
Syed Affan Ali <160457228+MrAfoo@users.noreply.github.com>
2025-01-28 15:33:14 +0500

    Fixes and added admin dashboard

--- src/app/api/createOrder/route.ts
+++ src/app/api/createOrder/route.ts
@@ -3,8 +3,15 @@ import { client } from '../../../sanity/lib/client';
 export async function POST(req: Request) {
     const orderData = await req.json();
     
-    const result = await client.create(orderData);
-    
+    // Add _type and timestamp to the order
+    const order = {
+        _type: 'order',
+        orderDate: new Date().toISOString(),
+        ...orderData
+    };
+   
+    const result = await client.create(order);
+   
     return new Response(JSON.stringify(result), {
         headers: { 'Content-Type': 'application/json' },
         status: 200
